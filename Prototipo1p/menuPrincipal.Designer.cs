f92359e37272539535777bb3d2b9ad2adb710d30
chipa3 <chipa_124578@outlook.com>
2020-08-25 13:44:30 -0600

    Contenedor MDI

---
+++ Prototipo1p/menuPrincipal.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Prototipo1p
+{
+    partial class menuPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aYUDAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cATALOGOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pROCESOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNFORMESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hERRAMIENTASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aYUDAToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aYUDAToolStripMenuItem,
+            this.cATALOGOSToolStripMenuItem,
+            this.pROCESOSToolStripMenuItem,
+            this.iNFORMESToolStripMenuItem,
+            this.hERRAMIENTASToolStripMenuItem,
+            this.aYUDAToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aYUDAToolStripMenuItem
+            // 
+            this.aYUDAToolStripMenuItem.Name = "aYUDAToolStripMenuItem";
+            this.aYUDAToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aYUDAToolStripMenuItem.Text = "ABRIR";
+            // 
+            // cATALOGOSToolStripMenuItem
+            // 
+            this.cATALOGOSToolStripMenuItem.Name = "cATALOGOSToolStripMenuItem";
+            this.cATALOGOSToolStripMenuItem.Size = new System.Drawing.Size(104, 24);
+            this.cATALOGOSToolStripMenuItem.Text = "CATALOGOS";
+            // 
+            // pROCESOSToolStripMenuItem
+            // 
+            this.pROCESOSToolStripMenuItem.Name = "pROCESOSToolStripMenuItem";
+            this.pROCESOSToolStripMenuItem.Size = new System.Drawing.Size(99, 24);
+            this.pROCESOSToolStripMenuItem.Text = "PROCESOS ";
+            // 
+            // iNFORMESToolStripMenuItem
+            // 
+            this.iNFORMESToolStripMenuItem.Name = "iNFORMESToolStripMenuItem";
+            this.iNFORMESToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.iNFORMESToolStripMenuItem.Text = "INFORMES";
+            // 
+            // hERRAMIENTASToolStripMenuItem
+            // 
+            this.hERRAMIENTASToolStripMenuItem.Name = "hERRAMIENTASToolStripMenuItem";
+            this.hERRAMIENTASToolStripMenuItem.Size = new System.Drawing.Size(131, 24);
+            this.hERRAMIENTASToolStripMenuItem.Text = "HERRAMIENTAS";
+            // 
+            // aYUDAToolStripMenuItem1
+            // 
+            this.aYUDAToolStripMenuItem1.Name = "aYUDAToolStripMenuItem1";
+            this.aYUDAToolStripMenuItem1.Size = new System.Drawing.Size(71, 24);
+            this.aYUDAToolStripMenuItem1.Text = "AYUDA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aYUDAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cATALOGOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pROCESOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iNFORMESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hERRAMIENTASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aYUDAToolStripMenuItem1;
+    }
+}
+
